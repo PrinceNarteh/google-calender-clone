8000543cb0d170ec5200f7792140bf22590f8c4d
Prince Narteh <prinarteh@gmail.com>
2022-07-31 15:25:49 +0000

    adds header

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { FcCalendar } from "react-icons/fc";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 const CalendarHeader = () => {
-  return <div className="bg-teal-500">Calendar Header</div>;
+  return (
+    <div className="px-4 py-2 flex items-center">
+      <FcCalendar className="mr-2 w-12 h-12" />
+      <h1 className="mr-10 text-3xl text-gray-400">MyCalendar</h1>
+      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button>
+        <AiOutlineLeft className="cursor-pointer text-gray-600 mx-2" />
+      </button>
+      <button>
+        <AiOutlineRight className="cursor-pointer text-gray-600 mx-2" />
+      </button>
+    </div>
+  );
 };
 
 export default CalendarHeader;
--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,13 +1,22 @@
+import dayjs from "dayjs";
 import React from "react";
 
 const Day = ({ day, rowIdx }) => {
+  const getCurrentDayClass = () => {
+    return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+      ? "bg-blue-600 text-white rounded-full w-7"
+      : "";
+  };
+  
   return (
     <div className="border border-gray-200 flex flex-col">
       <header className="flex flex-col items-center">
         {rowIdx === 0 && (
           <p className="text-sm mt-1">{day.format("ddd").toUpperCase()}</p>
         )}
-        <p className="text-sm p-1 my-1 text-center">{day.format("DD")}</p>
+        <p className={`text-sm p-1 my-1 text-center ${getCurrentDayClass()}`}>
+          {day.format("DD")}
+        </p>
       </header>
     </div>
   );
