e40f4084da8b8d2cbd88941bacb7bfd8d5cab329
Prince Narteh <prinarteh@gmail.com>
2022-07-31 15:52:53 +0000

    finish styling header component

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { FcCalendar } from "react-icons/fc";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import { useAppContext } from "../context/appContext";
+import dayjs from "dayjs";
 
 const CalendarHeader = () => {
   const { monthIndex, setMonthIndex } = useAppContext();
 
+  const handleReset = () => setMonthIndex(dayjs().month());
   const handlePrevMonth = () => setMonthIndex(monthIndex - 1);
   const handleNextMonth = () => setMonthIndex(monthIndex + 1);
 
@@ -13,13 +15,18 @@ const CalendarHeader = () => {
     <div className="px-4 py-2 flex items-center">
       <FcCalendar className="mr-2 w-12 h-12" />
       <h1 className="mr-10 text-3xl text-gray-400">MyCalendar</h1>
-      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button onClick={handleReset} className="border rounded py-2 px-4 mr-5">
+        Today
+      </button>
       <button onClick={handlePrevMonth}>
         <AiOutlineLeft className="cursor-pointer text-gray-600 mx-2" />
       </button>
       <button onClick={handleNextMonth}>
         <AiOutlineRight className="cursor-pointer text-gray-600 mx-2" />
       </button>
+      <h2 className="ml-4 text-xl text-gray-500 font-bold">
+        {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
+      </h2>
     </div>
   );
 };
